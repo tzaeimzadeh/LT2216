4791ec99d050e0609070d20d61322144476f67e4
Tala Zaeimzadeh <guszaeita@GU.GU.SE@eduserv.flov.gu.se>
2019-02-17 18:09:14 +0100

    updates

--- Lab_1/app.py
+++ Lab_1/app.py
@@ -16,3 +16,17 @@ def lab1():
     response = make_response(vxml)
     response.headers["Content-Type"] = "application/xml"
     return response
+
+@app.route('/book_flight')
+def book_flight():
+    vxml = render_template('book_flight.xml')
+    response = make_response(vxml)
+    response.headers["Content-Type"] = "application/xml"
+    return response
+
+@app.route('/delayed_flights')
+def delayed_flights():
+    vxml = render_template('delayed_flights.xml')
+    response = make_response(vxml)
+    response.headers["Content-Type"] = "application/xml"
+    return response
