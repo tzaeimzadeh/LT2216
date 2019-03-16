e34b4f11a77d6e81bdf6e77daede6e0446ebd056
Tala <Tala@Talas-Air.lan>
2019-03-17 00:26:17 +0100

    adding xml files, app.py and audio files

---
+++ final_project/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, make_response, send_from_directory
+import random
+
+app = Flask(__name__)
+
+song = ["song1", "song2", "song3", "song4", "song5", "song6"]
+
+
+@app.route('/menu')
+def menu():
+    vxml = render_template('menu.xml')
+    response = make_response(vxml)
+    response.headers["Content-Type"] = "application/xml"
+    return response
+
+
+@app.route('/guess_song')
+def guess_song():
+    vxml = render_template('guess_song.xml', form=random.choice(song))
+    response = make_response(vxml)
+    response.headers["Content-Type"] = "application/xml"
+    return response
+
+
+@app.route('/sing_verse')
+def sing_verse():
+    vxml = render_template('sing_verse.xml', form=random.choice(song))
+    response = make_response(vxml)
+    response.headers["Content-Type"] = "application/xml"
+    return response
